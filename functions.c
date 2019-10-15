d5e10957f8cefbbd5b6cedb29e8a544889c17d28
rageafk <j.skofelt@hotmail.com>
2019-10-15 12:11:54 +0300

    Taulukon arvojen keskiarvon ja varianssin lasku funktioita. Ei oo tosin testattu xd

--- functions.c
+++ functions.c
@@ -1,3 +1,9 @@
+//Tilakoneen eri tilat
+enum state { IDLE=1, READ_SENSOR, READ_MESSAGE, SEND_MESSAGE };
+//tilakoneen nykyinen tila
+enum state myState = IDLE;
+
+
 void addValuesToArray(float ax, float ay, float az, float bx, float by, float bz) {
     if (IndexNumber > 50) {
         IndexNumber = 0;
@@ -10,18 +16,18 @@ void calcVariance() {
     float mean, num;
     int i, j;
     mean = num = 0;
-    
+
     for (j = 1; j < 3; j++) {
         for (i = 0; i < 50; i++) {
             n += 1;
             mean += MPU_Data[i][j];
-        } 
-        
+        }
+
         mean = mean / n;
-        
+
         for (i = 0; i < 50; i++) {
-            
+
         }
     }
-    
+
 }
